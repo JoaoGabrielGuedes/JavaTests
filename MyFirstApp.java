4fb07b1a556a48992ea8fa5dc05caa35b83ad3ad
JoaoGabrielGuedes <jgabriel350@gmail.com>
2025-10-29 23:05:14 -0300

    print / println

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,5 +1,7 @@
 public class MyFirstApp {
   public static void main (String [] args) {
     System.out.print("Hello world!");
-  }
+    System.out.print(" É exibido na mesma linha que o hello world");
+    System.out.println("\nprintln só quebra a linha depois da impressão, então é necessário usar o \\n");
+    }
 }
